96c2333a0eb24b7b2ecdf81cf0553a92eaa4a363
apples-24x7-support-helpdesk <167751817+apples-24x7-support-helpdesk@users.noreply.github.com>
2024-05-01 19:23:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactButton').addEventListener('click', function() {
-    alert('Redirecting to Apple Support...');
-    // Redirect to support URL or handle as needed
-    window.location.href = 'https://support.apple.com';
-});
